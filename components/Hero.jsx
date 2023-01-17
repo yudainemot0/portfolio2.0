1136df793c85068878f0ec6a915a5b3d1a3a88cf
yudainemot0 <majesticsurfboard@gmail.com>
2023-01-17 12:12:21 +0900

    🎨 useTypewriterとJamsurfの写真修正

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -32,7 +32,7 @@ export default function Hero() {
                 <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
                     Web Developer
                 </h2>
-                <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+                <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold px-10">
                     <span className="mr-3">{text}</span>
                     <Cursor cursorColor="#F7AB0A" />
                 </h1>
