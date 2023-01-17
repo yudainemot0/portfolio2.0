91d9caeaefc1e0534d7e647ebfd3aca05b902f2b
yudainemot0 <majesticsurfboard@gmail.com>
2023-01-18 07:48:49 +0900

    🎨 レスポンシブ修正

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -36,7 +36,7 @@ export default function ContactMe() {
                     </span>
                 </h4>
 
-                <div className="space-y-10">
+                <div className="space-y-5 md:space-y-10">
                     <div className="flex items-center space-x-5 justify-center">
                         <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
                         <p className="text-2xl">+1234567890</p>
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -8,11 +8,11 @@ export default function Skills() {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
             className="h-screen flex flex-col relative text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
-            <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+            <h3 className="absolute top-20 md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
                 Skills
             </h3>
 
-            <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over skill for currency profiengy</h3>
+            <h3 className="absolute top-32 md:top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over skill for currency profiengy</h3>
 
             <div className="grid grid-cols-4 gap-4 md:gap-5">
                 <Skill />
